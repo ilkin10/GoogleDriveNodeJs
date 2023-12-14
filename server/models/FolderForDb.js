960a508e768ef07a45ae68f2ec61282cc6ed1a0d
ilkin10 <ilkinsuleymanov010@gmail.com>
2023-12-14 15:49:04 +0400

    Database Created SuccessFully (Structure)

--- server/models/FolderForDb.js
+++ server/models/FolderForDb.js
@@ -3,9 +3,10 @@ const mongoose = require('mongoose');
 const folderSchema = new mongoose.Schema({
   name: { type: String, required: true },
   owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  files: [{ type: mongoose.Schema.Types.ObjectId, ref: 'File' }], // Array of file references
 },
 {
-  timestamps:true
+  timestamps: true
 });
 
 const FolderForDb = mongoose.model('FolderForDb', folderSchema);
